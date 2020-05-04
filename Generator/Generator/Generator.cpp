2f71af27dc8e441f7c4c697e9996e48efa48cd22
RodrigoProjects <RodrigoProjects>
2020-05-04 19:37:32 +0100

    Generator changes - Rodirgo | 2020-05-04 19:37

--- Generator/Generator/Generator.cpp
+++ Generator/Generator/Generator.cpp
@@ -1,15 +1,15 @@
-// Generator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+ï»¿// Generator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "Generator.h"
-int nPatches=0;
+int nPatches = 0;
 
-class Point{       
-public:             
-    double x = 0;        
+class Point {
+public:
+    double x = 0;
     double y = 0;
     double z = 0;
-    Point() {   
+    Point() {
 
     }
     Point(double x1, double y1, double z1) {
@@ -19,18 +19,18 @@ public:
     }
 
     string pointToString() {
-        char* buff = (char *)malloc(sizeof(char) * 100);
-        snprintf(buff,100,"%.8f %.8f %.8f",x,y,z);
+        char* buff = (char*)malloc(sizeof(char) * 100);
+        snprintf(buff, 100, "%.8f %.8f %.8f", x, y, z);
         return buff;
     }
 };
 
-class Triangle{
+class Triangle {
 public:
     tuple <Point, Point, Point> coord;
 
     Triangle() {
-        Point p(0,0,0); 
+        Point p(0, 0, 0);
         coord = make_tuple(p, p, p);
     }
 
@@ -45,12 +45,12 @@ void trianglesToFile(vector<Triangle> t, string f) {
         int x = (rand() % 3 + 1), y = (rand() % 3), z = (rand() % 3);
         file << x << " " << y << " " << z << ",";
 
-        file << get<0>(t[i].coord).pointToString()<< "," << get<1>(t[i].coord).pointToString() << "," << get<2>(t[i].coord).pointToString() << endl;
+        file << get<0>(t[i].coord).pointToString() << "," << get<1>(t[i].coord).pointToString() << "," << get<2>(t[i].coord).pointToString() << endl;
         i++;
     }
 }
 
-void generatePlaneFile(double x,double z, string f) {
+void generatePlaneFile(double x, double z, string f) {
     double auxX = x / 2, auxZ = z / 2;
 
     //Pontos para Triangulo 1
@@ -70,7 +70,7 @@ void generatePlaneFile(double x,double z, string f) {
     //ordem original(p6,p5,p4) mas p4 = p1 e p5=p3
     Triangle t4(p6, p3, p1);
 
-    vector <Triangle> v = { t1,t2,t3,t4};
+    vector <Triangle> v = { t1,t2,t3,t4 };
 
     trianglesToFile(v, f);
 
@@ -80,13 +80,13 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
     double xn = x / (n + 1), yn = y / (n + 1), zn = z / (n + 1);
     double i, j, k;
     vector<Triangle> triangles;
-    
+
     //face 1 e 6 (laterais)
     for (j = 0; j < y; j += yn) {
         for (k = 0; k < z; k += zn) {
             //Face 1 Triangulo A
-            Point p1(0, j+yn, k+zn);
-            Point p2(0, j+yn, k);
+            Point p1(0, j + yn, k + zn);
+            Point p2(0, j + yn, k);
             Point p3(0, j, k);
 
             Triangle f1a(p1, p2, p3);
@@ -94,10 +94,10 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
 
             //Face 1 Triangulo B
 //          Point p4(0, j, k);
-            Point p5(0, j, k+zn);
-//          Point p6(0, j+yn, k+zn);
+            Point p5(0, j, k + zn);
+            //          Point p6(0, j+yn, k+zn);
 
-            //ordem original(p4,p5,p6) mas p6=p1 e p4=p3
+                        //ordem original(p4,p5,p6) mas p6=p1 e p4=p3
             Triangle f1b(p3, p5, p1);
             triangles.push_back(f1b);
 
@@ -112,9 +112,9 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
             //Face 6 Triangulo B
 //            Point p10(x, j + yn, k + zn);
             Point p11(x, j, k + zn);
-  //          Point p12(x, j , k);
+            //          Point p12(x, j , k);
 
-            //ordem original(p10,p11,p12) mas p12=p7 p10=p9
+                      //ordem original(p10,p11,p12) mas p12=p7 p10=p9
             Triangle f6b(p9, p11, p7);
             triangles.push_back(f6b);
 
@@ -126,24 +126,24 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
         for (i = 0; i < x; i += xn) {
             //Face 2 Triangulo A
             Point p1(i, y, k);
-            Point p2(i+ xn, y, k+zn);
-            Point p3(i+xn, y, k);
+            Point p2(i + xn, y, k + zn);
+            Point p3(i + xn, y, k);
 
             Triangle f2a(p1, p2, p3);
             triangles.push_back(f2a);
 
             //Face 2 Triangulo B
 //            Point p4(i, y, k);
-            Point p5(i, y, k+zn);
-  //          Point p6(i+xn, y, k+zn);
-            
-            //ordem original(p4,p5,p6) mas p4=p1 e p6=p2
+            Point p5(i, y, k + zn);
+            //          Point p6(i+xn, y, k+zn);
+
+                      //ordem original(p4,p5,p6) mas p4=p1 e p6=p2
             Triangle f2b(p1, p5, p2);
             triangles.push_back(f2b);
 
             //Face 4 Triangulo A
-            Point p7(i+xn, 0, k+zn);
-            Point p8(i, 0, k+zn);
+            Point p7(i + xn, 0, k + zn);
+            Point p8(i, 0, k + zn);
             Point p9(i, 0, k);
 
             Triangle f4a(p7, p8, p9);
@@ -151,10 +151,10 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
 
             //Face 4 Triangulo B
  //           Point p10(i, 0, k);
-            Point p11(i+xn, 0, k);
-   //         Point p12(i+xn, 0, k+zn);
+            Point p11(i + xn, 0, k);
+            //         Point p12(i+xn, 0, k+zn);
 
-            //ordem original(p10,p11,p12) mas p10=p9 e p12=p7
+                     //ordem original(p10,p11,p12) mas p10=p9 e p12=p7
             Triangle f4b(p9, p11, p7);
             triangles.push_back(f4b);
 
@@ -162,12 +162,12 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
     }
 
     //face 5 e 3
-    for (j = 0; j < y;j+=yn) {
-        for (i = 0; i < x;i+=xn) {
+    for (j = 0; j < y;j += yn) {
+        for (i = 0; i < x;i += xn) {
             //Face 5 Triangulo A
-            Point p1(i+xn, j, z);
-            Point p2(i+xn, j+yn, z);
-            Point p3(i, j+yn, z);
+            Point p1(i + xn, j, z);
+            Point p2(i + xn, j + yn, z);
+            Point p3(i, j + yn, z);
 
             Triangle f5a(p1, p2, p3);
             triangles.push_back(f5a);
@@ -183,18 +183,18 @@ void generateBoxFile(double x, double y, double z, long int n, string f) {
 
             //Face 3 Triangulo A
             Point p7(i, j, 0);
-            Point p8(i, j+yn, 0);
-            Point p9(i+xn, j, 0);
+            Point p8(i, j + yn, 0);
+            Point p9(i + xn, j, 0);
 
             Triangle f3a(p7, p8, p9);
             triangles.push_back(f3a);
 
             //Face 3 Triangulo B
        //     Point p10(i, j+yn, 0);
-            Point p11(i+xn, j+yn, 0);
-         //   Point p12(i+xn, j, 0);
+            Point p11(i + xn, j + yn, 0);
+            //   Point p12(i+xn, j, 0);
 
-            //ordem original(p10,p11,p12) p10=p8 p12=p9
+               //ordem original(p10,p11,p12) p10=p8 p12=p9
             Triangle f3b(p8, p11, p9);
             triangles.push_back(f3b);
         }
@@ -210,35 +210,35 @@ void generateSphereFile(double radius, int slices, int stacks, string f) {
     double stackSkew = PI / (stacks), sliceSkew = (2 * PI) / slices;
     for (i = 1; i <= stacks;i++) {
         double phi = (PI / 2) - (i * stackSkew);
-        for (j = 1; j <= slices ;j++) {
+        for (j = 1; j <= slices;j++) {
             double teta = j * sliceSkew;
 
             double previousX = radius * cos(phi) * sin(teta);
             double previousZ = radius * cos(teta) * cos(phi);
 
 
-                Point p1(radius * cos(phi + stackSkew) * sin(teta + sliceSkew), radius * sin(phi + stackSkew), radius * cos(teta + sliceSkew) * cos(phi + stackSkew));
-                Point p2(previousX, radius * sin(phi),previousZ);
-                Point p3(previousX * cos(sliceSkew) + previousZ * sin(sliceSkew), radius * sin(phi), -previousX * sin(sliceSkew) + previousZ * cos(sliceSkew));
+            Point p1(radius * cos(phi + stackSkew) * sin(teta + sliceSkew), radius * sin(phi + stackSkew), radius * cos(teta + sliceSkew) * cos(phi + stackSkew));
+            Point p2(previousX, radius * sin(phi), previousZ);
+            Point p3(previousX * cos(sliceSkew) + previousZ * sin(sliceSkew), radius * sin(phi), -previousX * sin(sliceSkew) + previousZ * cos(sliceSkew));
 
-                Triangle t(p1, p2, p3);
-                triangles.push_back(t);
-                double auxX = radius * cos(phi + stackSkew) * sin(teta + sliceSkew), auxZ = radius * cos(teta + sliceSkew) * cos(phi + stackSkew);
-                
-                //Point p4(previousX * cos(sliceSkew) + previousZ * sin(sliceSkew), radius * sin(phi), -previousX * sin(sliceSkew) + previousZ * cos(sliceSkew));
-                Point p5(auxX * cos(sliceSkew) + auxZ * sin(sliceSkew), radius * sin(phi + stackSkew), -auxX * sin(sliceSkew) + auxZ * cos(sliceSkew));
-                //Point p6(radius * cos(phi + stackSkew) * sin(teta + sliceSkew), radius * sin(phi + stackSkew), radius * cos(teta + sliceSkew) * cos(phi + stackSkew));
+            Triangle t(p1, p2, p3);
+            triangles.push_back(t);
+            double auxX = radius * cos(phi + stackSkew) * sin(teta + sliceSkew), auxZ = radius * cos(teta + sliceSkew) * cos(phi + stackSkew);
 
-                //ordem original(p4,p5,p6) mas p4=p3 e p6=p1
-                Triangle t1(p3, p5, p1);
-                triangles.push_back(t1);
-         
+            //Point p4(previousX * cos(sliceSkew) + previousZ * sin(sliceSkew), radius * sin(phi), -previousX * sin(sliceSkew) + previousZ * cos(sliceSkew));
+            Point p5(auxX * cos(sliceSkew) + auxZ * sin(sliceSkew), radius * sin(phi + stackSkew), -auxX * sin(sliceSkew) + auxZ * cos(sliceSkew));
+            //Point p6(radius * cos(phi + stackSkew) * sin(teta + sliceSkew), radius * sin(phi + stackSkew), radius * cos(teta + sliceSkew) * cos(phi + stackSkew));
+
+            //ordem original(p4,p5,p6) mas p4=p3 e p6=p1
+            Triangle t1(p3, p5, p1);
+            triangles.push_back(t1);
+
         }
     }
     trianglesToFile(triangles, f);
 }
 
-void torus(float iRadius, float eRadius, float slices, float stacks,string f) {
+void torus(float iRadius, float eRadius, float slices, float stacks, string f) {
     float x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4;
     float theta = 0, phi = 0;
     float sliceSkew = 2 * PI / slices;
@@ -267,7 +267,7 @@ void torus(float iRadius, float eRadius, float slices, float stacks,string f) {
             z4 = (iRadius + eRadius * cos(phi + stackSkew)) * sin(theta);
             y4 = eRadius * sin(phi + stackSkew);
 
-            
+
             Point p1(x1, y1, z1);
             Point p2(x2, y2, z2);
             Point p3(x3, y3, z3);
@@ -297,7 +297,7 @@ void generateConeFile(double radius, double height, double slices, double stacks
     double i, j;
     double ang, raio1, raio2, frst, scnd;
 
-    //fazer a circunferência da base
+    //fazer a circunferï¿½ncia da base
     for (i = 0; i < slices; i++) {
 
         ang = alpha * i;
@@ -364,7 +364,7 @@ bool checkRepeatedFiles(const char* file) {
     return false;
 }
 
-void updateXML(const char* file){
+void updateXML(const char* file) {
     XMLDocument doc;
     if (doc.LoadFile("Files.xml") == XML_SUCCESS) {
         if (checkRepeatedFiles(file)) {
@@ -404,7 +404,7 @@ float* bezier(float t, float* p0, float* p1, float* p2, float* p3) {
 
 float* bezierPatch(float u, float v, float** allPoints, int* index) {
     float* controlPoints[4];
-    for(int i=0;i<4;i++){
+    for (int i = 0;i < 4;i++) {
         int idd = index[4 * i];
 
         float* p0 = allPoints[index[4 * i]];
@@ -413,7 +413,7 @@ float* bezierPatch(float u, float v, float** allPoints, int* index) {
         float* p3 = allPoints[index[4 * i + 3]];
 
         float* point = bezier(u, p0, p1, p2, p3);
-        
+
         controlPoints[i] = point;
     }
     return bezier(v, controlPoints[0], controlPoints[1], controlPoints[2], controlPoints[3]);
@@ -428,29 +428,29 @@ void removeChar(std::string& str, char character)
 
 void writeResultPoints(int tecellationLevel, float** allPoints, int** index, string f) {
     vector<Triangle> triangles;
-    float inc = 1.0 / tecellationLevel;
-    float x, x1, y, y1;
-    
+    float step = 1.0 / tecellationLevel;
+    float u, u1, v, v1;
+
 
     for (int i = 0; i < nPatches; i++) {
 
         for (int j = 0; j < tecellationLevel; j++) {
             for (int k = 0; k < tecellationLevel; k++) {
-                x = inc * j;
-                x1 = inc * (j + 1);
-                y = inc * k;
-                y1 = inc * (k + 1);
+                u = step * j;
+                u1 = step * (j + 1);
+                v = step * k;
+                v1 = step * (k + 1);
 
-                float* aux = bezierPatch(x, y, allPoints, index[i]);
-                float* aux1 = bezierPatch(x, y1, allPoints, index[i]);
-                float* aux2 = bezierPatch(x1, y, allPoints, index[i]);
-                float* aux3 = bezierPatch(x1, y1, allPoints, index[i]);
+                float* aux = bezierPatch(u, v, allPoints, index[i]);
+                float* aux1 = bezierPatch(u, v1, allPoints, index[i]);
+                float* aux2 = bezierPatch(u1, v, allPoints, index[i]);
+                float* aux3 = bezierPatch(u1, v1, allPoints, index[i]);
 
                 Point pA(aux[0], aux[1], aux[2]);
                 Point pB(aux1[0], aux1[1], aux1[2]);
                 Point pC(aux2[0], aux2[1], aux2[2]);
                 Point pD(aux3[0], aux3[1], aux3[2]);
-                
+
                 Triangle t1(pC, pA, pB);
                 Triangle t2(pB, pD, pC);
                 triangles.push_back(t1);
@@ -465,15 +465,15 @@ void writeResultPoints(int tecellationLevel, float** allPoints, int** index, str
 
 int readBezier(ifstream file, char* fDest, int tecellationLevel) {
     int x = 0;
-    
+
     string line;
     if (file.is_open()) {
         getline(file, line);
         nPatches = atoi(line.c_str());
-        int** index = (int** )malloc(sizeof(int*) * nPatches);
+        int** index = (int**)malloc(sizeof(int*) * nPatches);
 
         for (int efe = 0; efe < nPatches; efe++) {
-            index[efe] = (int *) malloc(sizeof(int) * 16);
+            index[efe] = (int*)malloc(sizeof(int) * 16);
         }
 
         for (x = 0; x < nPatches;x++)
@@ -482,10 +482,10 @@ int readBezier(ifstream file, char* fDest, int tecellationLevel) {
             removeChar(line, ',');
 
             std::istringstream data(line.c_str());
-            int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+            int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
             data >> a >> b >> c >> d >> e >> f >> g >> h >> i >> j >> k >> l >> m >> n >> o >> p;
 
-            vector<int> *aux = new vector<int>();
+            vector<int>* aux = new vector<int>();
             *aux = { a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p };
 
             index[x] = aux->data();
@@ -503,8 +503,8 @@ int readBezier(ifstream file, char* fDest, int tecellationLevel) {
         for (int efe = 0; efe < nPoints; efe++) {
             allPoints[efe] = (float*)malloc(sizeof(float) * 3);
         }
-        
-        
+
+
         for (x = 0; x < nPoints; x++)
         {
             getline(file, line);
@@ -528,35 +528,35 @@ int readBezier(ifstream file, char* fDest, int tecellationLevel) {
 }
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 
-    if (strcmp(argv[1], "plane") == 0 && argc==5){
-        generatePlaneFile(atof(argv[2]),atof(argv[3]),argv[4]);
+    if (strcmp(argv[1], "plane") == 0 && argc == 5) {
+        generatePlaneFile(atof(argv[2]), atof(argv[3]), argv[4]);
         updateXML(argv[4]);
     }
-    else if (strcmp(argv[1], "box") == 0 && argc==6) {
+    else if (strcmp(argv[1], "box") == 0 && argc == 6) {
         //float x, float y, float z, float n, string f
         generateBoxFile(atof(argv[2]), atof(argv[3]), atoi(argv[4]), 0, argv[5]);
         updateXML(argv[5]);
     }
     else if (strcmp(argv[1], "box") == 0 && argc == 7) {
         //float x, float y, float z, float n, string f
-        generateBoxFile(atof(argv[2]), atof(argv[3]), atoi(argv[4]), atoi(argv[5]),argv[6]);
+        generateBoxFile(atof(argv[2]), atof(argv[3]), atoi(argv[4]), atoi(argv[5]), argv[6]);
         updateXML(argv[6]);
     }
-    else if (strcmp(argv[1], "sphere") == 0 && argc==6) {
+    else if (strcmp(argv[1], "sphere") == 0 && argc == 6) {
         generateSphereFile(atof(argv[2]), atoi(argv[3]), atoi(argv[4]), argv[5]);
         updateXML(argv[5]);
     }
-    else if (strcmp(argv[1], "cone") == 0 && argc==7) {
+    else if (strcmp(argv[1], "cone") == 0 && argc == 7) {
         generateConeFile(atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]), argv[6]);
         updateXML(argv[6]);
 
-    } 
+    }
     else if (strcmp(argv[1], "bezier") == 0 && argc == 5) {
         //bezier file tecellation destFile.txt
 
-        int res = readBezier(ifstream(argv[2]),argv[4], atoi(argv[3]));
+        int res = readBezier(ifstream(argv[2]), argv[4], atoi(argv[3]));
         if (res == -1) printf("Erro");
         else {
             updateXML(argv[4]);
